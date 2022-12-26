af0c4e6eba09b7a81c4d2d59828ef2ed8765d018
Xanax <Merwwj@gmail.com>
2022-12-26 21:07:43 +0330

    minor changes

--- script.js
+++ script.js
@@ -1,58 +1,132 @@
-document.addEventListener("DOMContentLoaded", main);
+// const btn = document.getElementById("bruh");
+// btn.addEventListener("click", function () {
+//   var pdfObject = jsPDFInvoiceTemplate.default(props); //returns number of pages created
+//   console.log("hello?");
+// });
 
-function main() {
-  /*
-    box - DIV
-        content - DIV
-            innerctx - DIV
-                title - H4
-                input - P
-            innerctx - DIV
-                title - H4
-                input - P
-        contentIMG - DIV
-            logo - IMG
-    */
-  const container = document.querySelector('div[class="container"')
-  const newBox = document.createElement("div");
-  const newContent = document.createElement("div");
-  const textContent = document.createElement("div");
-  const modelTitle = document.createElement("h4");
-  const modelName = document.createElement("p");
-  const textContent2 = document.createElement("div");
-  const modelTitle2 = document.createElement("h4");
-  const modelName2 = document.createElement("p");
-  const logo = document.createElement('div')
-  const logoImg = document.createElement('img')
+var props = {
+  outputType: jsPDFInvoiceTemplate.OutputType.Save,
+  returnJsPDFDocObject: true,
+  fileName: "Invoice 2021",
+  orientationLandscape: false,
+  compress: true,
+  logo: {
+    src: "./images/logo.jpg",
+    type: "PNG", //optional, when src= data:uri (nodejs case)
+    width: 30.66, //aspect ratio = width/height
+    height: 30.66,
+    margin: {
+      top: 0, //negative or positive num, from the current position
+      left: 0, //negative or positive num, from the current position
+    },
+  },
+  stamp: {
+    inAllPages: true, //by default = false, just in the last page
+    src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+    type: "JPG", //optional, when src= data:uri (nodejs case)
+    width: 20, //aspect ratio = width/height
+    height: 20,
+    margin: {
+      top: 0, //negative or positive num, from the current position
+      left: 0, //negative or positive num, from the current position
+    },
+  },
+  business: {
+    name: "VARTECH - وارتک",
+    address: "ورامین - پاساژ توحید - طبقه همکف پلاک 10",
+    phone: "09903303135 - 09019284117",
+    email: "FailureJunky@gmail.com",
+    email_1: "Vartechshop@gmail.com",
+    website: "www.vartech.gigfa.com",
+  },
+  contact: {
+    label: "Invoice issued for:",
+    name: "معراج حسینی",
+    address: "-",
+    phone: "09395118977",
+    email: "merwwj@gmail.com",
+    otherInfo: "imxanax.github.io",
+  },
+  invoice: {
+    label: "Invoice #: ",
+    num: 1,
+    invDate: "Payment Date: 01/01/2021 18:12",
+    invGenDate: "Invoice Date: 02/02/2021 10:17",
+    headerBorder: false,
+    tableBodyBorder: false,
+    header: [
+      {
+        title: "#",
+        style: {
+          width: 10,
+        },
+      },
+      {
+        title: "Title",
+        style: {
+          width: 30,
+        },
+      },
+      {
+        title: "Description",
+        style: {
+          width: 80,
+        },
+      },
+      { title: "Price" },
+      { title: "Quantity" },
+      { title: "Unit" },
+      { title: "Total" },
+    ],
+    table: Array.from(Array(10), (item, index) => [
+      index + 1,
+      "There are many variations ",
+      "Lorem Ipsum is simply dummy text dummy text ",
+      200.5,
+      4.5,
+      "m2",
+      400.5,
+    ]),
+    additionalRows: [
+      {
+        col1: "Total:",
+        col2: "145,250.50",
+        col3: "ALL",
+        style: {
+          fontSize: 14, //optional, default 12
+        },
+      },
+      {
+        col1: "VAT:",
+        col2: "20",
+        col3: "%",
+        style: {
+          fontSize: 10, //optional, default 12
+        },
+      },
+      {
+        col1: "SubTotal:",
+        col2: "116,199.90",
+        col3: "ALL",
+        style: {
+          fontSize: 10, //optional, default 12
+        },
+      },
+    ],
+    invDescLabel: "Invoice Note",
+    invDesc:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+  },
+  footer: {
+    text: "The invoice is created on a computer and is valid without the signature and stamp.",
+  },
+  pageEnable: true,
+  pageLabel: "Page ",
+};
 
-  newBox.classList.add('box')
-  newContent.classList.add('content')
-  textContent.classList.add('content-texts')
-  modelTitle.classList.add('content-social-name')
-  modelName.classList.add('content-social-username')
-  textContent2.classList.add('content-texts')
-  modelTitle2.classList.add('content-social-name')
-  modelName2.classList.add('content-social-username')
-  logo.classList.add('content-img')
-  
-  logoImg.setAttribute('src','/images/icons/10_instagram.png')
-  modelTitle.textContent = 'MODEL'
-  modelTitle2.textContent = 'PRICE'
-  modelName.textContent = 'I5 12400'
-  modelName2.textContent = '5,200,000'
-
-  newBox.appendChild(newContent)
-  newContent.appendChild(textContent)
-  textContent.appendChild(modelTitle)
-  textContent.appendChild(modelName)
-  newContent.appendChild(textContent2)
-  textContent2.appendChild(modelTitle2)
-  textContent2.appendChild(modelName2)
-  newBox.appendChild(logo)
-  logo.appendChild(logoImg)
-
-  container.appendChild(newBox)
-  
-  // console.log(newBox)
-  // console.log(container)
-}
+const allList = document.querySelectorAll("li");
+allList.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log("CLICKED:" + e);
+  });
+});
