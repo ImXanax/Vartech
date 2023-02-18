71570065ca5dc2d3a81a85cbd97d1cb0f54ea195
Xanax <Merwwj@gmail.com>
2023-02-18 20:30:07 +0330

    feat: add add to cart functionality

---
+++ models/cartSchema.js
@@ -0,0 +1,23 @@
+module.exports = function Cart(oldCart) {
+  this.items = oldCart.items || {};
+  this.totalQty = oldCart.totalQty || 0;
+  this.totalPrice = oldCart.totalPrice || 0;
+
+  this.add = function (item, id) {
+    let storedItem = this.items[id];
+    if (!storedItem) {
+      storedItem = this.items[id] = { item: item, qty: 0, price: 0 };
+    }
+    storedItem.qty++;
+    storedItem.price = storedItem.item.price * storedItem.qty;
+    this.totalQty++;
+    this.totalPrice += storedItem.item.price;
+  };
+  this.generateArray = function () {
+    let arr = [];
+    for (let id in this.items) {
+      arr.push(this.items[id]);
+    }
+    return arr;
+  };
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const Cart = require('../models/cartSchema')
 const Product = require("../models/productSchema");
 
 router.get("/", (req, res) => {
@@ -13,4 +13,19 @@ router.get("/config", (req, res) => {
   res.render("config");
 });
 
-module.exports = router
+router.get("/addcart/:id", (req, res) => {
+  let productId = req.params.id;
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function (err, product) {
+    if (err) {
+      return res.redirect("/");
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart)
+    res.redirect('/')
+  });
+});
+
+module.exports = router;
