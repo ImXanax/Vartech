9681dcdfb33b453d28290a6fcd995ce4e1e92b8d
Xanax <Merwwj@gmail.com>
2023-02-20 00:15:12 +0330

    feat: shoping cart add / remove item method

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Cart = require('../models/cartSchema')
+const Cart = require("../models/cartSchema");
 const Product = require("../models/productSchema");
 
 router.get("/", (req, res) => {
@@ -13,19 +13,56 @@ router.get("/config", (req, res) => {
   res.render("config");
 });
 
+router.get("/cart/", (req, res) => {
+  if (!req.session.cart) {
+    return res.render("cart", { products: null });
+  }
+  let cart = new Cart(req.session.cart);
+  res.render("cart", {
+    products: cart.generateArray(),
+    totalPrice: cart.totalPrice,
+  });
+});
+
 router.get("/addcart/:id", (req, res) => {
   let productId = req.params.id;
   let cart = new Cart(req.session.cart ? req.session.cart : {});
 
   Product.findById(productId, function (err, product) {
     if (err) {
+      console.log(`ERR: ${err}`);
       return res.redirect("/");
     }
-    cart.add(product, product.id);
+    cart.add(product, product.id, product.imgSrc);
     req.session.cart = cart;
-    console.log(req.session.cart)
-    res.redirect('/')
+    console.log(req.session.cart);
+    res.redirect("/");
   });
+});
+
+router.get("/add/:id", (req, res) => {
+  let productId = req.params.id;
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function (err, product) {
+    if (err) {
+      console.log(`ERR: ${err}`);
+      return res.redirect("/");
+    }
+    cart.add(product, product.id, product.imgSrc);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect("/cart/");
+  });
+});
+
+router.get("/rem/:id", (req, res) => {
+  let productId = req.params.id;
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.rem(productId);
+  req.session.cart = cart;
+  res.redirect("/cart/");
 });
 
 module.exports = router;
